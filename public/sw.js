d45b2bee0281d5baa9aa765419605745a075ce1e
Matheus Nunes <matheus.o.nunes@gmail.com>
2019-10-01 20:02:59 -0300

    Always revalidate cache from root and JS

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /\/$/,
-  new workbox.strategies.StaleWhileRevalidate()
+  '/', new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
   /\.js$/,
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.StaleWhileRevalidate()
 );
 
 workbox.routing.registerRoute(
