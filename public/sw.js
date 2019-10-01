039a9afbc1293c43e74150ad9c0c459f9e3d2e85
Matheus Nunes <matheus.o.nunes@gmail.com>
2019-09-30 22:28:54 -0300

    Use indexed DB to persist and read TODOs

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,33 @@
+self.importScripts('idb.js')
+
 self.addEventListener('fetch', (e) => {
-  if(e.request.url.indexOf('/todos') != -1) {
-    e.respondWith(new Response('[{"title":"Teste"}]'))
+  if(e.request.method == 'GET' && e.request.url.indexOf('/todos') != -1) {
+    let newResponse = caches.open('pwa-todo').then((cache) => {
+      return cache.match(e.request).then((response) => {
+        if(response) return response
+        return fetch(e.request).then((fetchedResponse) => {
+          cache.put(e.request, fetchedResponse.clone())
+          return fetchedResponse
+        })
+      })
+    })
+
+    e.respondWith(newResponse)
   }
 })
+
+self.addEventListener('activate', (e) => {
+  console.log('service worker activated ')
+  e.waitUntil(createDb())
+})
+
+createDb = () => {
+  console.log('opening DB!!')
+  idb.openDB('pwa_todo', 1, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+      console.log(db)
+      if(!db.objectStoreNames.contains('todos'))
+        db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true })
+    }
+  })
+}
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,33 +1,81 @@
 import React, { Component } from 'react';
+import { openDB } from 'idb';
 
 class TodoList extends Component {
   constructor(props) {
     super(props)
-    this.state = { todos: [] }
+    this.state = {
+      todos: [],
+      new_todo_title: ''
+    }
   }
 
   render() {
     return(
-      <div className="todos">
-        {this.state.todos.map((todo) => {
-          return(
-            <div className="todo">
-              <span>{todo.title}</span>
-            </div>
-          )
-        })}
-      </div>
+      <main>
+        <div className="new-todo-wrapper">
+          <form id="new-todo" onSubmit={this.handleSubmit.bind(this)}>
+            <label htmlFor="todo_title">Título</label>
+            <input
+              id="todo_title" type="text"
+              onChange={(event) => { this.setState({ new_todo_title: event.target.value }) }}
+              value={this.state.new_todo_title} />
+            <input type="submit" value="Criar TODO" />
+          </form>
+        </div>
+
+        <div className="todos">
+          {this.state.todos.map((todo, i) => {
+            return(
+              <div className="todo" key={`todo-${i}`}>
+                <span>{todo.title}</span>
+              </div>
+            )
+          })}
+        </div>
+      </main>
     )
   }
 
   componentDidMount() {
-    window.fetch('/todos')
+    this.getTodos().
+      then((todos) => { this.setState({ todos: todos }) })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const title = this.state.new_todo_title
+    this.setState({
+      todos: [...this.state.todos, { title: title }],
+      new_todo_title: ''
+    })
+
+    fetch('http://localhost:3000/todos', {
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({ title: title })
+    })
       .then((response) => {
         return response.json()
       })
-      .then((todos) => {
-        this.setState({ todos: todos })
+      .then((response) => {
+        this.insertTodo({id: response.id, title: title})
       })
+
+    // TODO: Should assert that IndexedDB is available on browser
+  }
+
+  async insertTodo(todo) {
+    const db = await openDB('pwa_todo', 1)
+    await db.add('todos', todo);
+  }
+
+  async getTodos() {
+    const db = await openDB('pwa_todo', 1)
+    return await db.getAll('todos')
   }
 }
 
