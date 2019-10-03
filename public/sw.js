510755a255f1a865e93abe52f422346386ba25c7
Matheus Nunes <matheus.o.nunes@gmail.com>
2019-10-03 19:06:42 -0300

    Fix workbox strategy syntax

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  '/', new workbox.strategies.networkFirst()
+  '/', new workbox.strategies.NetworkFirst()
 )
 
 workbox.routing.registerRoute(
