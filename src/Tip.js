73405f2d48a93f111262e4373d3d42f764180a43
Matheus Nunes <matheus.o.nunes@gmail.com>
2019-10-03 21:04:33 -0300

    Add cool transition for tip change

--- src/Tip.js
+++ src/Tip.js
@@ -11,29 +11,64 @@ class Tip extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { tip: this.randomFrom(tips) }
+
+    let available_tips = [...tips]
+    const tip = this.randomFrom(tips)
+    const tip_index = tips.indexOf(tip)
+    let next_available_tips = [...tips]
+    const next_tip_index = tips.indexOf(tip)
+    next_available_tips.splice(next_tip_index, 1)
+    const next_tip = this.randomFrom(next_available_tips)
+
+    this.state = {
+      tip: tip,
+      next_tip: next_tip
+    }
   }
 
   render() {
     return(
-      <div style={styles.tip_wrapper} onClick={this.getRandomTip.bind(this)}>
+      <div style={styles.tip_wrapper} onClick={this.nextTip.bind(this)}>
         <Icon path={mdiLightbulbOnOutline} size={1}
               style={{fill: 'white'}} />
-        <span style={styles.tip}>{this.state.tip}</span>
+        <div style={{overflow: 'hidden'}} ref={(ref) => this.tipWrapper = ref}>
+          <div style={{display: 'flex', flexDirection: 'column', transition: 'transform 0.1s ease-out', willChange: 'transform'}} ref={(ref) => this.currentTipInnerWrapper = ref}>
+            <span ref={(ref) => this.currentTipRef = ref} style={styles.tip}>{this.state.tip}</span>
+            <span ref={(ref) => this.nextTipRef = ref} style={styles.tip}>{this.state.next_tip}</span>
+          </div>
+        </div>
       </div>
     )
   }
 
   componentDidMount() {
-    this.getRandomTip()
+    this.tipWrapper.style.height = `${this.currentTipRef.offsetHeight}px`
+    if(this.currentTipRef.offsetHeight > this.nextTipRef.offsetHeight)
+      this.nextTipRef.style.height = `${this.currentTipRef.offsetHeight}px`
+    else
+      this.currentTipRef.style.height = `${this.nextTipRef.offsetHeight}px`
   }
 
-  getRandomTip() {
+  nextTip() {
     let available_tips = [...tips]
-    const current_tip_index = tips.indexOf(this.state.tip)
+    const current_tip_index = tips.indexOf(this.state.next_tip)
     available_tips.splice(current_tip_index, 1)
-    const tip = this.randomFrom(available_tips)
-    this.setState({ tip: tip })
+    let tip = this.randomFrom(available_tips)
+    this.tipWrapper.style.height = `${this.nextTipRef.offsetHeight}px`
+    this.currentTipInnerWrapper.style.transform = `translateY(-${this.currentTipRef.offsetHeight}px)`
+    setTimeout(() => {
+      this.currentTipInnerWrapper.style.transition = 'none'
+      this.currentTipInnerWrapper.style.transform = `translateY(0px)`
+
+      this.setState({ tip: this.state.next_tip, next_tip: tip }, () => {
+        this.currentTipInnerWrapper.style.transition = 'transform 0.1s ease-out'
+      })
+    }, 100)
+
+    if(this.currentTipRef.offsetHeight > this.nextTipRef.offsetHeight)
+      this.nextTipRef.style.height = `${this.currentTipRef.offsetHeight}px`
+    else
+      this.currentTipRef.style.height = `${this.nextTipRef.offsetHeight}px`
   }
 
   randomFrom(array) {
@@ -49,9 +84,12 @@ const styles = {
   },
   tip: {
     fontSize: 14,
-    marginLeft: 12,
+    marginLeft: 16,
     color: 'white',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    flexShrink: 0,
+    display: 'flex',
+    alignItems: 'center'
   }
 }
 
