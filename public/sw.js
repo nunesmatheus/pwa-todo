7580423600fa19742ade7be8235f980d0adc2c18
Matheus Nunes <matheus.o.nunes@gmail.com>
2019-10-03 21:06:43 -0300

    Add reminder

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+// TODO: Add update notification!!!
+
 workbox.routing.registerRoute(
   '/', new workbox.strategies.NetworkFirst()
 )
